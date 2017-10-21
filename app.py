b250829e8279bc320bc87d037ee3a46865e919f3
Benjamin Keen <benalexkeen@gmail.com>
2017-10-21 17:22:39 +0100

    Add area, spline, line and multiple line charts

--- app.py
+++ app.py
@@ -23,5 +23,46 @@ def simple_line():
     }
     return render_template("simple_line.html", data=data)
 
+
+@app.route("/simple-step/")
+def simple_step():
+    df = pd.read_csv(os.path.join(data_dir, 'simple_line.csv'))
+    records = df.to_dict(orient='records')
+    data = {
+        'graph_data': json.dumps(records)
+    }
+    return render_template("simple_step.html", data=data)
+
+
+@app.route("/simple-spline/")
+def simple_spline():
+    df = pd.read_csv(os.path.join(data_dir, 'simple_line.csv'))
+    records = df.to_dict(orient='records')
+    data = {
+        'graph_data': json.dumps(records)
+    }
+    return render_template("simple_spline.html", data=data)
+
+
+@app.route("/multiple-line/")
+def multiple_line():
+    df = pd.read_csv(os.path.join(data_dir, 'multiple_line.csv'))
+    records = df.to_dict(orient='records')
+    data = {
+        'graph_data': json.dumps(records)
+    }
+    return render_template("multiple_line.html", data=data)
+
+
+@app.route("/simple-area/")
+def simple_area():
+    df = pd.read_csv(os.path.join(data_dir, 'simple_line.csv'))
+    records = df.to_dict(orient='records')
+    data = {
+        'graph_data': json.dumps(records)
+    }
+    return render_template("simple_area.html", data=data)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
